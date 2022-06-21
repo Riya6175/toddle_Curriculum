2bdb36b73768fa65c81a3c63520a8f9b71de4791
Riya6175 <soniriya243@gmail.com>
2022-06-21 22:43:19 +0530

    ui

--- toddle/src/components/Child_component/Child_component.js
+++ toddle/src/components/Child_component/Child_component.js
@@ -5,14 +5,20 @@ import {RiDeleteBin6Fill} from 'react-icons/ri';
 import {data} from "../../data";
 
 export default function Child_component(props) {
-  const inputRef = useRef(null);
+  //const inputRef = useRef(null);
 
-    // console.log(props.props.data)
+    // console.log(props.setShowProp)
     // console.log(props.props.level)
 
+    const dragFunction = () => {
+      props.setShowProp(Math.floor((Math.random() * 1000) + 1))
+      
+    }
+
   return (
-    <>
-    <div>
+    <> 
+    <div onMouseUp={dragFunction} >
+    {/* onMouseUp={props.props.useShow(Math.floor((Math.random() * 1000) + 1))} */}
       <div className="child">
       <div className='icons'>
           <BiMove/>
@@ -25,7 +31,7 @@ export default function Child_component(props) {
       </div>
 
       <div className='Text'>
-          {props.props && <input type="text" id="topic" ref={inputRef} className={props.props.level === 0 ? 'Topic' : props.props.level === 1 ? 'topic_child' : 'topic_subchild'} name="Topic" placeholder="Text" size="2500" defaultValue= {props.props.data}></input>}
+          {props.props && <input type="text" id="topic"  className={props.props.level === 0 ? 'Topic' : props.props.level === 1 ? 'topic_child' : 'topic_subchild'} name="Topic" placeholder="Text" size="2500" defaultValue= {props.props.data}></input>}
           {/* <input type="text" id="Topic" name="Topic" placeholder="Text" size="2500" defaultValue={"props.props.data"}></input> */}
       </div>
       </div>
--- toddle/src/components/parent_component/Parent_component.js
+++ toddle/src/components/parent_component/Parent_component.js
@@ -1,5 +1,6 @@
 import React,{useState, useEffect, useRef} from "react";
 import { Draggable } from "react-drag-reorder";
+import { nanoid } from 'nanoid'
 import Child_component from "../Child_component/Child_component";
 import {MdAddCircleOutline} from "react-icons/md";
 import { BiMove,BiRightArrowAlt,BiLeftArrowAlt } from 'react-icons/bi';
@@ -8,53 +9,69 @@ import "./parent.css"
 
 export default function Parent_component() {
 
+  const dataset = 
+  [{ïd:1,"data":"first", "level":0},
+   {ïd:2,"data":"first-child","level":1},
+   {ïd:3,"data":"seocnd-child","level":1},
+   {ïd:4,"data":"firstchild2","level":2},
+   {ïd:5,"data":"second","level":0},
+   {ïd:6,"data":"second-child","level":1}
+]
+
   const [standard, setStandard] = useState([]);
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(0);
   const inputRef = useRef(null);
 
   useEffect(() => {
     // console.log(document.getElementById('childElements'))
-    // var clsElements = document.querySelectorAll("input");
-    console.log(inputRef)
+     var clsElements = document.querySelectorAll("#topic");
+     console.log("ce",clsElements);
+     console.log(inputRef)
     // console.log("current",inputRef.current.childNodes)
-    const newlist = inputRef.current.childNodes
+    //const newlist = inputRef.current.childNodes
     // console.log(newlist)
     // console.log("hello",inputRef.current.children[0])
     //console.log(trial)
     //console.log(inputRef.current.childNodes[1].childNodes[0].childNodes[0].childNodes[2].childNodes[0].defaultValue)
 
-  }, [])
+  }, [show])
 
-  const deleteItem = (index) => {
-   setShow(false);
-  }
 
   const TextComponent = () =>{
     return (
     <>
+    <Draggable>
         <div className="child">
         <div className='icons'>
             <BiMove/>
             <BiLeftArrowAlt/>
             <BiRightArrowAlt/>
-            <RiDeleteBin6Fill onClick={deleteItem}/>
+            <RiDeleteBin6Fill/>
         </div>
         <div className='box-child'>
             &nbsp;
         </div>
         <div className='Text-child'>
-            <input type="text" id="topic" className="Topic" name="Topic" placeholder="Text" size="2500"></input>
+            <input type="text" id="topic-t" className="Topic" name="Topic" placeholder="Text" size="2500"></input>
         </div>
         </div>
         <hr></hr>
+        </Draggable>
     </>
     )
   }
 
   
   const onAddStandard = event => {
+    setShow(show + 1)
     setStandard(standard.concat(<TextComponent key={standard.length} />));
   };
+
+  const childClickFunction = (e) => {
+    // e.preventDefault();
+    console.log("Function Running")
+    setShow(show + 1)
+  }
 
 
   return (
@@ -85,25 +102,35 @@ export default function Parent_component() {
         <hr></hr>
         <div id="childElements" className="dragFlex">
           <Draggable>
-            <Child_component props={{"data":"first", "level":0}}/>
-            <Child_component props={{"data":"first-child","level":1}}/>
-            <Child_component props={{"data":"seocnd-child","level":1}}/>
-            <Child_component props={{"data":"firstchild2","level":2}}/>
-            <Child_component props={{"data":"second","level":0}}/>
-            <Child_component props={{"data":"second-child","level":1}}/>
-            <Child_component props={{"data":"third","level":0}}/>
-          </Draggable>
-        </div>
+            {dataset.map((data) => {
+            return (
+              <>
+              <div className="child" key={data.id}>
+              <div className='icons'>
+                  <BiMove/>
+                  <BiLeftArrowAlt/>
+                  <BiRightArrowAlt/>
+                  <RiDeleteBin6Fill/>
+              </div>
+              <div className={data.level === 0 ? 'box' : data.level === 1 ? 'box-child' : 'box-child2'}>
+                  &nbsp;
+              </div>
         
-        {/* <div className="dragFlex"> */}
-
-        {/* </div> */}
-        {standard}
+              <div className='Text'>
+                  {data.data && <input type="text" id="topic"  className={data.level === 0 ? 'Topic' : data.level === 1 ? 'topic_child' : 'topic_subchild'} name="Topic" placeholder="Text" size="2500" defaultValue= {data.data}></input>}
+                  {/* <input type="text" id="Topic" name="Topic" placeholder="Text" size="2500" defaultValue={"props.props.data"}></input> */}
+              </div>
+              </div>
+              <hr></hr>
+              </>
+            )})}
+          </Draggable>
+          {standard}
+        </div>
         <button className="add-button" onClick={onAddStandard}><a>
           <MdAddCircleOutline style={{"fontSize":"1.3rem","paddingRight":"2%"}}/>
           Add a Standard</a>
         </button>
-        
     </div>
   )
 }
